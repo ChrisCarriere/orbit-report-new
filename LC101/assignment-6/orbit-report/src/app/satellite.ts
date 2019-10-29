f1f2c59db7ef5630ba32228f6ffa350f09bcf043
Chris Carriere <chris.carriere323@gmail.com>
2019-10-28 21:02:40 -0500

    Completed part 6, highlighting space debris

--- LC101/assignment-6/orbit-report/src/app/satellite.ts
+++ LC101/assignment-6/orbit-report/src/app/satellite.ts
@@ -10,5 +10,13 @@ export class Satellite {
         this.launchDate = launchDate,
         this.orbitType = orbitType,
         this.operational = operational
-     }
+    }
+
+    shouldShowWarning() {
+        if(this.type.toLowerCase() === "space debris"){
+            return true
+        }else {
+            return false
+        }
+    }
 }
